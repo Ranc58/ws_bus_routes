7e7cdebeb650ea5ab867c23f786cdae0ac36b978
Ranc58 <rancvova@gmail.com>
2019-11-27 20:57:41 +0300

    Fix trio channels for clone

--- fake_bus.py
+++ fake_bus.py
@@ -37,23 +37,37 @@ async def send_updates(server_address, receive_channel):
     async with open_websocket_url(server_address) as ws:
         async with receive_channel:
             async for value in receive_channel:
-                print(value)
                 await ws.send_message(value)
                 await trio.sleep(0.1)
 
 
-async def main():
-    routes_counter = 0
-    async with trio.open_nursery() as nursery:
+async def get_channels(nursery, sockets_count):
+    send_channels = []
+    for _ in range(sockets_count):
         send_channel, receive_channel = trio.open_memory_channel(0)
         async with send_channel, receive_channel:
-            nursery.start_soon(send_updates,  'ws://127.0.0.1:8080', receive_channel.clone())
-            for route in load_routes():
-                if routes_counter < 6:
-                    random_bus_index = random.randint(99, 9999)
-                    random_bus_id = generate_bus_id(route['name'], random_bus_index)
-                    nursery.start_soon(run_bus, send_channel.clone(), random_bus_id, route)
-                    routes_counter +=1
+            nursery.start_soon(send_updates, 'ws://127.0.0.1:8080', receive_channel.clone())
+            send_channels.append(send_channel.clone())
+    return send_channels
+
+
+async def main():
+    async with trio.open_nursery() as nursery:
+        routes_count = 20
+        buses_count = 5
+        sockets_count = 10
+        processed_routes = 0
+        channels = await get_channels(nursery, sockets_count)
+        routes_count_d = 0
+        for route in load_routes(routes_count=routes_count):
+            routes_count_d += 1
+            for t in range(buses_count):
+                send_channel = random.choice(channels)
+                random_bus_index = random.randint(99, 9999)
+                random_bus_id = generate_bus_id(route['name'], random_bus_index)
+                nursery.start_soon(run_bus, send_channel, random_bus_id, route)
+                processed_routes += 1
+            print(processed_routes)
 
 
 try:
