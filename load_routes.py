8dcf8d28c157074a916583469b21e7c564a36347
Ranc58 <rancvova@gmail.com>
2019-11-27 20:57:25 +0300

    Add routes counter for loading from file

--- load_routes.py
+++ load_routes.py
@@ -2,24 +2,19 @@ import json
 import os
 
 
-def load_routes(directory_path='routes'):
-    for filename in os.listdir(directory_path):
-        if filename.endswith(".json"):
-            filepath = os.path.join(directory_path, filename)
-            with open(filepath, 'r', encoding='utf8') as file:
-                yield json.load(file)
+def get_route_info(filename, directory_path):
+    if filename.endswith(".json"):
+        filepath = os.path.join(directory_path, filename)
+        with open(filepath, 'r', encoding='utf8') as file:
+            yield json.load(file)
 
 
-# def process_routes():
-#     t = 0
-#     routes = 0
-#     for route in load_routes():
-#         if t >= 10:
-#             t = 0
-#             yield routes
-#         t +=1
-#         routes +=1
-
-
-# for s in process_routes():
-#     print(s)
+def load_routes(directory_path='routes', routes_count=None):
+    if routes_count:
+        counter = 0
+        for filename in os.listdir(directory_path):
+            for route in get_route_info(filename, directory_path):
+                if counter >= routes_count:
+                    return
+                counter += 1
+                yield route
